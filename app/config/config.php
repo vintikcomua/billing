c455a0a97a6504ac4b573a67d7680f2e82e288ae
Vintik <vintikcomua@gmail.com>
2016-07-17 22:08:17 +0300

    Setting database connection

--- app/config/config.php
+++ app/config/config.php
@@ -8,7 +8,7 @@ return new \Phalcon\Config(array(
         'host'        => 'localhost',
         'username'    => 'root',
         'password'    => '',
-        'dbname'      => 'test',
+        'dbname'      => 'billing',
         'charset'     => 'utf8',
     ),
     'application' => array(
